3e66c7b7657bf3dddb26d2c75ca89fbc1184c19f
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-09 13:48:30 +0200

    part 2, Fetching Data from an API with Async/Await, done

---
+++ src/asyncAwait-Part1-Part2.ts
@@ -0,0 +1,52 @@
+//Part 1 - Refactoring Your Own Promise with Async/Await 
+
+const myOutlookDay = new Promise((resolve, reject) => {
+
+    let isThisAGoodDay: number = Math.random();
+    //instead of > 0.5 and thus random 50/50, I changed the value. Otherwise this gets too depressive :) 
+    if (isThisAGoodDay > 0.7) {
+        resolve('This is a good day. Enjoy!');
+    } else {
+        reject('This is a bad day. Go back to sleep.');
+    }
+});
+
+const outlookDay = async () => {
+    try {
+        const value = await myOutlookDay;
+        console.log(value);
+    } catch (error) {
+        console.log(error);
+    }
+};
+outlookDay();
+
+//Part 2: Fetching Data from an API with Async/Await 
+
+type AdviceResponse = {
+    slip: {
+        id: number,
+        advice: string,
+    };
+};
+
+const fetchAdvideByIDs = async (id: number) => {
+    try {
+        const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        const data = await response.json();
+        const advice: string = data.slip.advice;
+        console.log(`Advice ${id}: `, advice);
+    }  catch (error) {
+        console.log(error);
+    }  
+};
+
+//max ID appers to be no 224
+//id: draw a random number between 1 and 224, for advice
+
+let id: number = Math.floor(Math.random() * 224) + 1;
+fetchAdvideByIDs(id);
+id = Math.floor(Math.random() * 224) + 1;
